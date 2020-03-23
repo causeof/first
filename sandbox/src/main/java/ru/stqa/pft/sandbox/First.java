37af36fbaa73be20c22b019d9ee9f78a7d082716
Misha <misha.kma@mail.ru>
2020-03-23 17:22:34 +0300

    Add some functional

--- sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -2,7 +2,28 @@ package ru.stqa.pft.sandbox;
 
 public class First {
 
-	public static void main(String[] args){
-		System.out.println("Hello world!");
+	public static void main(String[] args) {
+		hello("world");
+		double len = 5;
+		System.out.println("Ploshchad' kvadrata so storonoy " + len + " = " + area(len));
+
+		double a = 4;
+		double b = 6;
+		System.out.println(("Ploshchad' treugol'nika " + a + " and " + b + " = " + area (a,b)));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area (double l)	{
+		return l * l;
+	}
+
+	public static double area (double a, double b){
+		return a * b;
 	}
 }
+
+
+
