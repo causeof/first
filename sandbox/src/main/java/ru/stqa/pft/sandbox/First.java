4857d3db93021fe8c32c6b87dfa960bea79406d1
Misha <misha.kma@mail.ru>
2020-03-23 17:39:14 +0300

    Add classes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -4,24 +4,24 @@ public class First {
 
 	public static void main(String[] args) {
 		hello("world");
-		double len = 5;
-		System.out.println("Ploshchad' kvadrata so storonoy " + len + " = " + area(len));
 
-		double a = 4;
-		double b = 6;
-		System.out.println(("Ploshchad' treugol'nika " + a + " and " + b + " = " + area (a,b)));
+		Square s = new Square(5);
+		System.out.println("Ploshchad' kvadrata so storonoy " + s.l + " = " + area(s));
+
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println(("Ploshchad' treugol'nika " + r.a + " and " + r.b + " = " + area (r)));
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public static double area (double l)	{
-		return l * l;
+	public static double area (Square s)	{
+		return s.l * s.l;
 	}
 
-	public static double area (double a, double b){
-		return a * b;
+	public static double area (Rectangle r){
+		return r.a * r.b;
 	}
 }
 
