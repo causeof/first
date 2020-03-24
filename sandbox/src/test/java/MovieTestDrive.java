1c080fdc682ffc754e513ebd16d168303619c5fa
Misha <misha.kma@mail.ru>
2020-03-24 15:28:42 +0300

    GuessGame added

---
+++ sandbox/src/test/java/MovieTestDrive.java
@@ -0,0 +1,16 @@
+public class MovieTestDrive {
+  public static void main(String[] args){
+    Movie one = new Movie();
+    one.title = "How to burned";
+    one.genre = "Tragedy";
+    one.rating = -2;
+    Movie two = new Movie();
+    two.title = "Forgot in office";
+    two.genre = "Comedy";
+    two.rating = 5;
+    Movie three = new Movie();
+    three.title = "Fighting Club";
+    three.genre = "Drama";
+    three.rating = 127;
+  }
+}
