22cff182348bcad03a150d633917738449f1de98
Misha <misha.kma@mail.ru>
2020-03-24 15:59:30 +0300

    DrumKit added

---
+++ sandbox/src/test/java/DrumKit.java
@@ -0,0 +1,12 @@
+public class DrumKit {
+  boolean topHat = true;
+  boolean snare = true;
+
+
+  void playSnare() {
+    System.out.println("Bang! Bang! Bang!");
+  }
+  void playTopHat() {
+    System.out.println("Thin Thin Thin");
+  }
+}
